882bc5a9dee388236974d80e67de118f96f0953e
Johann Scott <johannscott16@gmail.com>
2024-01-23 15:18:58 +0000

    shell colours, neofetch, misc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+function fish_prompt
+    printf '╭─%s@%s : %s\n' $USER $hostname (pwd | sed "s/\/home\/$USER/~/")
+    printf '╰─λ '
+end
+
+function fish_greeting
+    clear
+    neofetch
+end
